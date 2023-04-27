b653dd9cee21fd12c1d2bc1a04ded1320ceffca8
Arthit LungYa <rthit096@gmail.com>
2023-04-27 10:57:50 +0700

    latest

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const productController = require('../controllers/productController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router
+  .route('/') //verifyJWT,
+  .get(productController.getAllProducts)
+  .post(productController.createNewProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct)
+
+router.route('/:id').get(productController.getAllProducts)
+
+module.exports = router
